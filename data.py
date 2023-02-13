e60149c32f15cd69a2cf752e119cff6e51d80985
Sergio Madrigal Muelas <smadrigal@unicef.org>
2023-02-13 14:21:28 +0100

    new: create data.py and define structure

---
+++ data.py
@@ -0,0 +1,17 @@
+# Data Sanitizer
+# ----
+# Version 1.0
+
+def sanitizer_title():
+    # Function
+    return True
+
+
+def sanitizer_text():
+    # Function
+    return True
+
+
+def sanitizer_ids():
+    # Function
+    return True
