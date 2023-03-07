91dced1986a076f099debc8213ccc20311b8cb44
mdgdeveloper <se.rgiomadrigal@gmail.com>
2023-03-07 21:11:12 +0100

    update: Program ready to add the data

--- legscrapper/data-sanitize/dataSanitize.py
+++ legscrapper/data-sanitize/dataSanitize.py
@@ -1,11 +1,15 @@
 # Data Sanitizer
 from sanitizer import *
 
-# Test
-muncipality = "Sotorribas"
-base_api = f"https://data.opendatasoft.com/api/records/1.0/search/?dataset=georef-spain-municipio%40public&q={muncipality}&facet=year&facet=acom_code&facet=acom_name&facet=prov_code&facet=prov_name&facet=mun_code&facet=mun_name&facet=mun_area_code&facet=mun_type"
 
-test = get_json_from_api(base_api)
+def locationData(village):
+    village = village.lower()
+    print(f"Village: {village}")
+    base_api = f"https://data.opendatasoft.com/api/records/1.0/search/?dataset=georef-spain-municipio%40public&q={village}&facet=year&facet=acom_code&facet=acom_name&facet=prov_code&facet=prov_name&facet=mun_code&facet=mun_name&facet=mun_area_code&facet=mun_type"
+    data = get_json_from_api(base_api)
+    result = ["", ""]
+    if (data["records"]):
+        result = [data["records"][0]["fields"]["prov_name"],
+                  data["records"][0]["fields"]["acom_name"]]
 
-print(test["records"][0]["fields"]["prov_name"], "-",
-      test["records"][0]["fields"]["acom_name"])
+    return (result)
