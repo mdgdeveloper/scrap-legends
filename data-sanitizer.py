04595339061c6ec856885c83f0d357b93ab30f6a
sergiomadrigal <sergiomadrigal@gmail.com>
2023-03-30 07:48:59 +0200

    data: Data Sanitizer script started

---
+++ data-sanitizer.py
@@ -0,0 +1,9 @@
+# Data Sanitizer 
+# Version: 0.1
+# Created by Sergio Madrigal
+# ------
+
+
+def array_to_string(textArray):
+    # 
+    return True
--- legscrapper/data-sanitize/sanitizer.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-# Basic function to request API 
-def get_json_from_api(api_url):
-    response = requests.get(api_url)
-    json_response = response.json()
-    return json_response
-
